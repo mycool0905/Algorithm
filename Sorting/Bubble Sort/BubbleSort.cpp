e53fab39ff92c2277968d8efe1cf718233f350d2
unknown <mycool0905@gmail.com>
2019-08-05 22:14:17 +0900

    2019-08-05-20:14 > Bubble Sort

--- Sorting/Bubble Sort/BubbleSort.cpp
+++ Sorting/Bubble Sort/BubbleSort.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
-#define INF 987654321
 
 using namespace std;
 
 /* Bubble Sort(버블정렬)은 간단하다. */
 /* 반복문 돌면서 두 개씩 비교하는데 큰 숫자가 앞에 있다면 */
 /* 바로 뒤의 숫자와 swap 해준다. */
+
+/* 버블 정렬의 시간 복잡도는 O(N^2) */
 
 void printArray(int arr[]) {
 	for (int i = 0; i < 10; i++)
@@ -22,19 +23,17 @@ void swap(int* a, int* b) {
 }
 
 /* 자기 배열의 내부에서 소팅 */
-void selectionSort2(int arr[]) {
-	int min;
-	int index = 0;
+void bubbleSort(int arr[]) {
 
 	for (int i = 0; i < 10; i++) {
-		min = INF;
-		for (int j = i; j < 10; j++) {
-			if (arr[j] < min) {
-				min = arr[j];
-				index = j;
+		for (int j = 0; j < 9-i; j++) {
+			/* 여기서 j는 왜 9-i까지 하느냐, */
+			/* Bubble Sort는 한 회전마다 제일 큰 숫자를 맨 뒤로 보내면서 진행하는 정렬이다.*/
+			/* 즉, 한 회전마다 어차피 맨 뒤 숫자가 제일 크니까 앞에 것들이랑 비교할 필요가 없다.*/
+			if (arr[j] > arr[j+1]) {
+				swap(arr[j], arr[j + 1]);
 			}
 		}
-		swap(arr[i], arr[index]);
 	}
 
 	printArray(arr);
@@ -42,10 +41,9 @@ void selectionSort2(int arr[]) {
 
 int main() {
 
-	int arr1[10] = { 1, 10, 5, 8, 7, 6 ,4 ,3 ,2 ,9 };
-	int arr2[10] = { 1, 10, 5, 8, 7, 6 ,4 ,3 ,2 ,9 };
-	selectionSort1(arr1);
-	selectionSort2(arr2);
+	int arr[10] = { 1, 10, 5, 8, 7, 6 ,4 ,3 ,2 ,9 };
+
+	bubbleSort(arr);
 
 	return 0;
 }
