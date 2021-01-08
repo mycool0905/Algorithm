63d5418111f63ee2e0d6e334db433c2d6a2d5145
kim.wangi <kim.wangi@navercorp.com>
2021-01-08 17:44:52 +0900

    Check If Two String Arrays are Equivalent

---
+++ LeetCode/Kotlin/Check_If_Two_String_Arrays_are_Equivalent/Solution.kt
@@ -0,0 +1,8 @@
+package Check_If_Two_String_Arrays_are_Equivalent
+
+// URI: https://leetcode.com/explore/challenge/card/january-leetcoding-challenge-2021/580/week-2-january-8th-january-14th/3597/
+
+class Solution {
+    fun arrayStringsAreEqual(word1: Array<String>, word2: Array<String>): Boolean =
+        word1.joinToString("") == word2.joinToString("")
+}
