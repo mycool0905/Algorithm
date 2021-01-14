b8e8e484d623342e163bcf5a0eb7d58f14cc4e84
kim.wangi <kim.wangi@navercorp.com>
2021-01-14 18:04:31 +0900

    Minimum Operations to Reduce X to Zero

---
+++ LeetCode/Kotlin/Minimum_Operations_to_Reduce_X_to_Zero/Solution.kt
@@ -0,0 +1,40 @@
+package Minimum_Operations_to_Reduce_X_to_Zero
+
+// URI: https://leetcode.com/explore/challenge/card/january-leetcoding-challenge-2021/580/week-2-january-8th-january-14th/3603/
+
+class Solution {
+    fun minOperations(nums: IntArray, x: Int): Int {
+        val find = nums.sum() - x
+        if (find < 0) return -1
+
+        var first = 0
+        var last = 0
+        var sum = 0
+        var maxCnt = Int.MIN_VALUE
+
+        while (last != nums.size) {
+            when {
+                sum < find -> {
+                    sum += nums[last++]
+                }
+                sum == find -> {
+                    maxCnt = maxOf(maxCnt, last - first)
+                    sum += nums[last++]
+                }
+                else -> {
+                    sum -= nums[first++]
+                }
+            }
+        }
+
+        if(sum==find){
+            maxCnt = maxOf(maxCnt, last - first)
+        }
+
+        return if (maxCnt == Int.MIN_VALUE) -1 else nums.size - maxCnt
+    }
+}
+
+fun main(args:Array<String>){
+    Solution().minOperations(intArrayOf(5,2,3,1,1),5)
+}
