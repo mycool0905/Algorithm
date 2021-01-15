6a0f0ce5cd7d9ec98a2949b914e0ebf289b1d244
WanGi Kim <43199318+mycool0905@users.noreply.github.com>
2021-01-15 11:11:18 +0900

    Diagonal Traverse

--- LeetCode/Kotlin/Diagonal_Traverse/Solution.kt
+++ LeetCode/Kotlin/Diagonal_Traverse/Solution.kt
@@ -1,5 +1,6 @@
 package Diagonal_Traverse
 
+// Diagonal Traverse
 // URI: https://leetcode.com/explore/challenge/card/december-leetcoding-challenge/572/week-4-december-22nd-december-28th/3580/
 
 enum class Direction(val dx: Int, val dy: Int) {
