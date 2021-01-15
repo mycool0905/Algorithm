4cf9b004045a776f66e631e93e039e36ce033006
kim.wangi <kim.wangi@navercorp.com>
2021-01-15 17:28:03 +0900

    Get Maximum in Generated Array

---
+++ LeetCode/Kotlin/Get_Maximum_in_Generated_Array/Solution.kt
@@ -0,0 +1,26 @@
+package Get_Maximum_in_Generated_Array
+
+// URI: https://leetcode.com/explore/challenge/card/january-leetcoding-challenge-2021/581/week-3-january-15th-january-21st/3605/
+
+class Solution {
+    fun getMaximumGenerated(n: Int): Int {
+        if(n==0) return 0
+        if(n==1) return 1
+
+        var answer = Int.MIN_VALUE
+        val arr = IntArray(n+1)
+        arr[0] = 0
+        arr[1] = 1
+
+        for(i in 2..n) {
+            if (i % 2 == 0) {
+                arr[i] = arr[i / 2]
+            } else {
+                arr[i] = arr[i / 2 + 1] + arr[i / 2]
+            }
+            answer = maxOf(answer,arr[i])
+        }
+
+        return answer
+    }
+}
