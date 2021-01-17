b41875934780c446d91d1589f90f94a15d47fa8e
WanGi Kim <43199318+mycool0905@users.noreply.github.com>
2021-01-17 22:33:33 +0900

    Count Sorted Vowel Strings

--- LeetCode/Kotlin/Count_Sorted_Vowel_Strings/Solution.kt
+++ LeetCode/Kotlin/Count_Sorted_Vowel_Strings/Solution.kt
@@ -1,5 +1,6 @@
 package Count_Sorted_Vowel_Strings
 
+// Count Sorted Vowel Strings
 // URI: https://leetcode.com/explore/challenge/card/january-leetcoding-challenge-2021/581/week-3-january-15th-january-21st/3607/
 
 class Solution {
