50e571f9d39f01664ed8269d904fd2baf4a33d74
kim.wangi <kim.wangi@navercorp.com>
2021-01-20 17:15:46 +0900

    Valid Parentheses

---
+++ LeetCode/Kotlin/Valid_Parentheses/Solution.kt
@@ -0,0 +1,22 @@
+package Valid_Parentheses
+
+// URI: https://leetcode.com/explore/challenge/card/january-leetcoding-challenge-2021/581/week-3-january-15th-january-21st/3610/
+
+import java.util.Stack
+
+class Solution {
+    fun isValid(s: String): Boolean {
+        val stack = Stack<Char>()
+
+        for (char in s) {
+            when (char) {
+                '(', '[', '{' -> stack.push(char)
+                ')' -> if (stack.isEmpty() || stack.pop() != '(') return false
+                ']' -> if (stack.isEmpty() || stack.pop() != '[') return false
+                '}' -> if (stack.isEmpty() || stack.pop() != '{') return false
+            }
+        }
+
+        return stack.isEmpty()
+    }
+}
