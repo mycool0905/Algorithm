fcf4cc6a8c64689c14dbca9b87448afd3d7d8a28
kim.wangi <kim.wangi@navercorp.com>
2021-01-25 17:15:28 +0900

    Check If All 1's Are at Least Length K Places Away

---
+++ LeetCode/Kotlin/Check_If_All_1's_Are_at_Least_Length_K_Places_Away/Solution.kt
@@ -0,0 +1,26 @@
+package `Check_If_All_1's_Are_at_Least_Length_K_Places_Away`
+
+// URI: https://leetcode.com/explore/challenge/card/january-leetcoding-challenge-2021/582/week-4-january-22nd-january-28th/3616/
+
+class Solution {
+    fun kLengthApart(nums: IntArray, k: Int): Boolean {
+        var startBool = false
+        var before = 0
+
+        for (curIdx in nums.indices) {
+            if (nums[curIdx] == 1) {
+                if (startBool.not()) {
+                    startBool = true
+                    before = curIdx
+                } else {
+                    if(curIdx-before <= k){
+                        return false
+                    }
+                    before = curIdx
+                }
+            }
+        }
+
+        return true
+    }
+}
