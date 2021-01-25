e299e3799ce48c35778ab74543fcc2d216412552
WanGi Kim <43199318+mycool0905@users.noreply.github.com>
2021-01-25 17:16:34 +0900

    Check If All 1's Are at Least Length K Places Away

--- LeetCode/Kotlin/Check_If_All_1's_Are_at_Least_Length_K_Places_Away/Solution.kt
+++ LeetCode/Kotlin/Check_If_All_1's_Are_at_Least_Length_K_Places_Away/Solution.kt
@@ -1,5 +1,6 @@
 package `Check_If_All_1's_Are_at_Least_Length_K_Places_Away`
 
+// Check If All 1's Are at Least Length K Places Away
 // URI: https://leetcode.com/explore/challenge/card/january-leetcoding-challenge-2021/582/week-4-january-22nd-january-28th/3616/
 
 class Solution {
